838815e8dfa907d74b1b15dbd92fe2cc654322c6
AVDudarenko <aleksandr.dudareko@gmail.com>
2022-10-04 14:13:02 +0300

    format line

--- src/main/java/homeTaskThree/MethodsClass.java
+++ src/main/java/homeTaskThree/MethodsClass.java
@@ -1,7 +1,6 @@
 package homeTaskThree;
 
 public class MethodsClass {
-
 	synchronized public void reduceNumber(int number) {
 		for (int i = 0; i < 10; i++) {
 			System.out.println(Thread.currentThread().getName() + " " + (number - i));
